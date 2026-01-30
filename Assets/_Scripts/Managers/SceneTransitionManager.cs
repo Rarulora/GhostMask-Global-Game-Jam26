769d958c43162e80ee02df7201adc9e9e8a6fbb0
krefikk <ahmetrefikks@gmail.com>
2026-01-30 21:05:53 +0300

    Added Collectable Logic

--- Assets/_Scripts/Managers/SceneTransitionManager.cs
+++ Assets/_Scripts/Managers/SceneTransitionManager.cs
@@ -1,16 +1,59 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class SceneTransitonManager : MonoBehaviour
+public class SceneTransitionManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static SceneTransitionManager Instance;
+
+    private List<Animator> animators;
+    private bool uiDirty = true;
+
+    public const string MAIN_MENU_SCENE = "MainMenu";
+    public  const string GAMEPLAY_SCENE = "Gameplay";
+    public const string COSMETICS_SCENE = "Cosmetics";
+
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+        GetUIElements();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        
+        uiDirty = true;
+        GetUIElements();
+        foreach (var animator in animators)
+        {
+            animator.SetBool("Open", true);
+        }
+    }
+
+    private void GetUIElements()
+    {
+        animators = FindObjectsByType<Animator>(FindObjectsSortMode.None).ToList();
+        foreach (var animator in animators)
+        {
+            if (animator.GetComponent<Menu>() != null)
+                animators.Remove(animator);
+        }
+
+        uiDirty = false;
+    }
+    public void OpenScene(string name)
+    {
+        foreach (var animator in animators)
+        {
+            animator.SetBool("Open", false);
+        }
+
+        SceneManager.LoadScene(name);
     }
 }
--- Assets/_Scripts/Player/PlayerController.cs
+++ Assets/_Scripts/Player/PlayerController.cs
@@ -2,15 +2,58 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private int goldCollected = 0;
+    private int xpCollected = 0;
+
+    [Header("Collectable")]
+    public float collectRadius = 2f;
+    public LayerMask collectableLayer;
+
+    private void Update()
     {
-        
+        CheckForCollectables();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void IncreaseGold(int amount = 1)
     {
-        
+        goldCollected += amount;
+    }
+
+    public void IncreaseXP(int amount = 1)
+    {
+        xpCollected += amount;
+    }
+
+    public void DecreaseGold(int amount = 1)
+    {
+        goldCollected -= amount;
+        if (goldCollected < 0)
+            goldCollected = 0;
+    }
+
+    public void DecreaseXP(int amount = 1)
+    {
+        xpCollected -= amount;
+        if (xpCollected < 0)
+            xpCollected = 0;
+    }
+
+    private void CheckForCollectables()
+    {
+        Collider[] hitColliders = new Collider[10];
+        int numColliders = Physics.OverlapSphereNonAlloc(transform.position, collectRadius, hitColliders, collectableLayer);
+
+        for (int i = 0; i < numColliders; i++)
+        {
+            Collectable collectable = hitColliders[i].GetComponent<Collectable>();
+            if (collectable != null)
+                collectable.MarkAsDetected();
+        }
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, collectRadius);
     }
 }
