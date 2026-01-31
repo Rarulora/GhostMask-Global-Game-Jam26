782818b8ffb652e6f71b902c123cd4108fc1efb5
krefikk <ahmetrefikks@gmail.com>
2026-01-31 17:24:11 +0300

    Skill Tree System

--- Assets/_Scripts/Skills/SkillTreeManager.cs
+++ Assets/_Scripts/Skills/SkillTreeManager.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Linq;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -21,58 +23,32 @@ public class SkillTreeManager : MonoBehaviour
     {
         foreach (var attackNode in attackNodes)
         {
-            if (GameManager.Instance.SaveData.gainedSkillIDs.Contains<int>(attackNode.skill.ID))
+            if (attackNode.skill != null && GameManager.Instance.SaveData.gainedSkillIDs.Contains<int>(attackNode.skill.ID))
                 attackNode.gained = true;
         }
 
         foreach (var movementNode in movementNodes)
         {
-            if (GameManager.Instance.SaveData.gainedSkillIDs.Contains<int>(movementNode.skill.ID))
+            if (movementNode.skill != null && GameManager.Instance.SaveData.gainedSkillIDs.Contains<int>(movementNode.skill.ID))
                 movementNode.gained = true;
         }
 
         foreach (var healthNode in healthNodes)
         {
-            if (GameManager.Instance.SaveData.gainedSkillIDs.Contains<int>(healthNode.skill.ID))
+            if (healthNode.skill != null && GameManager.Instance.SaveData.gainedSkillIDs.Contains<int>(healthNode.skill.ID))
                healthNode.gained = true;
         }
 
         foreach (var maskNode in maskNodes)
         {
-            if (GameManager.Instance.SaveData.gainedSkillIDs.Contains<int>(maskNode.skill.ID))
+            if (maskNode.skill != null && GameManager.Instance.SaveData.gainedSkillIDs.Contains<int>(maskNode.skill.ID))
                 maskNode.gained = true;
         }
 
         foreach (var economyNode in economyNodes)
         {
-            if (GameManager.Instance.SaveData.gainedSkillIDs.Contains<int>(economyNode.skill.ID))
+            if (economyNode.skill != null && GameManager.Instance.SaveData.gainedSkillIDs.Contains<int>(economyNode.skill.ID))
                 economyNode.gained = true;
         }
-    }
-}
-
-public class SkillTreeNode : MonoBehaviour
-{
-    public int ID;
-    public SkillData skill;
-    public bool gained = false;
-
-    public void Purchase()
-    {
-        gained = true;
-
-        int[] newGainedSkills = new int[GameManager.Instance.SaveData.gainedSkillIDs.Length + 1];
-        int i;
-        for (i = 0; i < GameManager.Instance.SaveData.gainedSkillIDs.Length; i++) 
-        {
-            newGainedSkills[i] = GameManager.Instance.SaveData.gainedSkillIDs[i];
-        }
-
-        newGainedSkills[i] = skill.ID;
-        SaveData newSaveData = GameManager.Instance.SaveData;
-        newSaveData.gainedSkillIDs = newGainedSkills;
-
-        GameManager.Instance.SetSaveData(newSaveData);
-        GameManager.Instance.SaveGame();
     }
 }
