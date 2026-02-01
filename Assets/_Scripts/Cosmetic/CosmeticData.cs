7c60030bf1d2883a30268245a99daf16906c0866
Rarulora <emrebuluts054@gmail.com>
2026-02-01 03:58:46 +0300

    Add Cosmetic UI

--- Assets/_Scripts/Cosmetic/CosmeticData.cs
+++ Assets/_Scripts/Cosmetic/CosmeticData.cs
@@ -3,13 +3,11 @@ using UnityEngine;
 public enum CosmeticBuyMethod
 {
     Gold,
-    Achievement
 }
 
 public enum CosmeticType
 {
     Hat,
-    Wing
 }
 
 [CreateAssetMenu(fileName = "Cosmetic Data", menuName = "Database/CosmeticData")]
@@ -18,8 +16,7 @@ public class CosmeticData : ScriptableObject
     public int ID;
     public string Name;
     public string description;
-    public float price; // achievement ise 0
-    public string achievementID; // gold ise null/empty
+    public float price;
     public Sprite sprite;
     public CosmeticBuyMethod method;
     public CosmeticType type;
